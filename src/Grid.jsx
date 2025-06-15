5f687eb21e1a6746987e63536da3d4dd4dd1e73e
boda213-alaa-mohmad <alaaabdelrhman213@gmail.com>
2025-06-15 23:40:44 +0300

    adding content of html page

---
+++ src/Grid.jsx
@@ -0,0 +1,101 @@
+// import { Fragment } from "react";
+export default function Grid() {
+  return (
+    <div className="project">
+      <header className="header">
+        <h1>Our services</h1>
+      </header>
+      <div className="container">
+        <div className="grid-card">
+          <img
+            src="https://logospng.org/download/html-5/logo-html-5-768.png"
+            alt=""
+          />
+          <h3>HTML 5</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis
+            similique repudiandae, voluptatem optio nisi molestias.
+          </p>
+          <a href="https://www.w3schools.com/html/" class="button">
+            Read More
+          </a>
+        </div>
+
+        <div className="grid-card">
+          <img
+            src="https://th.bing.com/th/id/OIP.aiII04uB8m611vQPSw7HfgHaHa?r=0&rs=1&pid=ImgDetMain"
+            alt=""
+          />
+          <h3>CSS</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis
+            similique repudiandae, voluptatem optio nisi molestias.
+          </p>
+          <a href="https://www.w3schools.com/css/" class="button">
+            Read More
+          </a>
+        </div>
+
+        <div className="grid-card">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/027/127/560/original/javascript-logo-javascript-icon-transparent-free-png.png"
+            alt=""
+          />
+          <h3>Javascript</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis
+            similique repudiandae, voluptatem optio nisi molestias.
+          </p>
+          <a href="https://www.w3schools.com/js" class="button">
+            Read More
+          </a>
+        </div>
+
+        <div className="grid-card">
+          <img
+            src="https://th.bing.com/th/id/R.698519595d5bbc042a250fc356b4d12c?rik=gbCWwaLCVyHz9w&pid=ImgRaw&r=0"
+            alt=""
+          />
+          <h3>Sass</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis
+            similique repudiandae, voluptatem optio nisi molestias.
+          </p>
+          <a href="https://www.w3schools.com/sass" class="button">
+            Read More
+          </a>
+        </div>
+
+        <div className="grid-card">
+          <img
+            src="https://w7.pngwing.com/pngs/843/442/png-transparent-jquery-plain-logo-icon.png"
+            alt=""
+          />
+          <h3>JQuery</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis
+            similique repudiandae, voluptatem optio nisi molestias.
+          </p>
+          <a href="https://www.w3schools.com/jquery/" class="button">
+            Read More
+          </a>
+        </div>
+
+        <div className="grid-card">
+          <img
+            src="https://th.bing.com/th/id/R.f81a6f373c244b1f70f4b7402b5ab372?rik=rbXh4ieLuKt%2bmA&riu=http%3a%2f%2flogos-download.com%2fwp-content%2fuploads%2f2016%2f09%2fReact_logo_logotype_emblem.png&ehk=QhGOkKcUKCU7FBQgHOajOiJqJBACUTD2Ni6LsfqzCEA%3d&risl=&pid=ImgRaw&r=0"
+            alt=""
+          />
+          <h3>React</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis
+            similique repudiandae, voluptatem optio nisi molestias.
+          </p>
+          <a href="https://www.w3schools.com/react/" class="button">
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
